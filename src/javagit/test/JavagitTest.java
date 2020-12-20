c48b7f2e79a94c208e7c575c0fe9e9ee8ec756b1
Amina Yehia <aminasaad1218@gmail.com>
2020-12-20 09:36:44 +0200

    pass

--- src/javagit/test/JavagitTest.java
+++ src/javagit/test/JavagitTest.java
@@ -21,6 +21,8 @@ public class JavagitTest {
     
         System.out.println("Hello, World!"); 
         System.out.println("amina"); 
+                System.out.println("123"); 
+
         
     }
 
